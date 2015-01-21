e54b0619c1d3bb980c573e51ca18c3f9c9ef8d66
ingmap <ingmap@ingmap-netbook>
2015-01-21 08:21:43 -0430

    #1 Version inicial de prueba

---
+++ NetBeansProjects/SciServerAlpha/src/sciserver/server/SciServer.java
@@ -0,0 +1,22 @@
+/*
+ * TEsta clase Java es Software Libre y se distribuye gratuitamente bajo
+ * licencia GPL, por lo que está permitida su copia o distribución bajo los
+ * criterios de la misma.
+ * Ante cualquier problema, consulta, sugerencia, etc, puede escribir a
+ * darkpriestrelative@gmail.com, o dejar un mensaje en el sitio
+ * http://jingmap.blogspot.com
+ * Para descargar actualizaciones u obtener mayor documentación consulte la
+ * ayuda en línea del programa o visite la página oficial del proyecto
+ * (http://jingmap.blogspot.com).
+ *
+ * Copyleft 2010 Darkest Priest - (Manuel Pérez P. darkpriestrelative@gmail.com)
+ */
+package sciserver.server;
+
+/**
+ *
+ * @author ingmap
+ */
+public class SciServer {
+    
+}
---
+++ NetBeansProjects/SciServerAlpha/src/sciserveralpha/SciServer.java
@@ -0,0 +1,29 @@
+/*
+ * TEsta clase Java es Software Libre y se distribuye gratuitamente bajo
+ * licencia GPL, por lo que está permitida su copia o distribución bajo los
+ * criterios de la misma.
+ * Ante cualquier problema, consulta, sugerencia, etc, puede escribir a
+ * darkpriestrelative@gmail.com, o dejar un mensaje en el sitio
+ * http://jingmap.blogspot.com
+ * Para descargar actualizaciones u obtener mayor documentación consulte la
+ * ayuda en línea del programa o visite la página oficial del proyecto
+ * (http://jingmap.blogspot.com).
+ *
+ * Copyleft 2010 Darkest Priest - (Manuel Pérez P. darkpriestrelative@gmail.com)
+ */
+package sciserveralpha;
+
+/**
+ *
+ * @author ingmap
+ */
+public class SciServer {
+
+    /**
+     * @param args the command line arguments
+     */
+    public static void main(String[] args) {
+        // TODO code application logic here
+    }
+    
+}
